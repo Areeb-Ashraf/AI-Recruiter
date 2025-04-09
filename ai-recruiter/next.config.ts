f853c379b0a933683e2e1bf6cec228111a852c28
Ashraf, Areeb <areebashraf@my.unt.edu>
2025-04-09 15:36:21 -0500

    configs

--- ai-recruiter/next.config.ts
+++ ai-recruiter/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Ignore the node_pre_gyp issue
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mapbox/node-pre-gyp': false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
