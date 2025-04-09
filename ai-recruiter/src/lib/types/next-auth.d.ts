03256adc164d4fc7aeb93bc425634c87a2baafb1
Ashraf, Areeb <areebashraf@my.unt.edu>
2025-04-09 15:36:35 -0500

    next auth

---
+++ ai-recruiter/src/lib/types/next-auth.d.ts
@@ -0,0 +1,17 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+import { UserRole } from "@prisma/client";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: UserRole;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "@auth/core/jwt" {
+  interface JWT {
+    role?: UserRole;
+  }
+} 
